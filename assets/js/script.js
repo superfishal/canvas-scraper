b3c0714ff3e8e6dd2d23b333797689271f197451
superfishal <97133716+superfishal@users.noreply.github.com>
2022-12-05 20:23:33 -0800

    api GET works in insomnia not on browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,52 @@
-import * as dotenv from "dotenv";
-dotenv.config();
-import express from "express";
+// import * as dotenv from "dotenv";
+// dotenv.config();
+// import express from "express";
 let accessToken =
   "15042~5aPm1Li9BUEEM1HSraSURhxy4yByeNJ8yW9KGLnPDpdR5V7DUzwFbIVGqNS3z2VW";
 
-let canvasApiUrl = `https://courses.bootcampspot.com/api/v1/users/37897/courses?access_token=${accessToken}`;
+let canvasApiUrl =
+  "https://courses.bootcampspot.com/api/v1/courses/2756/users?access_token=15042~5aPm1Li9BUEEM1HSraSURhxy4yByeNJ8yW9KGLnPDpdR5V7DUzwFbIVGqNS3z2VW";
 
 fetch(canvasApiUrl, {
   mode: "no-cors",
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((error) => console.log(error));
+}).then(
+  function (res) {
+    if (res.status == 200 && res.ok) {
+      console.log("Promise resolved");
+      return res.json();
+    }
+    console.log("Promise rejected");
+    throw "promise rejected";
+  },
+  function (rej) {
+    console.log("promise rejected");
+    throw "promise rejected";
+  }
+);
+// fetch(canvasApiUrl, {
+//   mode: "no-cors",
+//   header: "GET",
+// })
+//   .then((response) => console.log(response))
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
 
+// try {
+//   const result = JSON.parse("");
+//   console.log(result);
+// } catch (err) {
+//   // 👇️ SyntaxError: Unexpected end of JSON input
+//   console.log("error", err);
+// }
+
+// This is for the UofO course # and my User #
 //   https://courses.bootcampspot.com/courses/2756/users/37897
+
+// this finds all users for a course
+// https://courses.bootcampspot.com/api/v1/courses/2756/users
+
+// then get the most recent grade for each of them and sort into groups..
+//   Get a users most recently graded submissions /v1/users/{id}/graded_submissions
+
+// list assignments for users
+// /api/v1/users/:user_id/courses/:course_id/assignments
